cf9c4a7a3864582bed22e5b61895776aa015ac29
Fauz <lemaiyanfauz@gmail.com>
2026-08-28 10:54:01 +0300

    Fix backend startup without database URL

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -23,7 +23,10 @@ def create_app():
     # DATABASE
     # ============================================================
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///qrib.db",
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # ============================================================
